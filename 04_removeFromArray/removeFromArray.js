5f94dcd0390a330b110e2690535be954c92c7909
alicecha <110170331+alicecha@users.noreply.github.com>
2023-01-17 18:29:11 +0200

    Implement function that removes n elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, targets) {
+    //create an array to store all target values to be removed
+    let removedArray = [];
 
+    //create loop that puts all the arguments after 'target' into an array
+    for (i = 1; i < arguments.length; i++) {
+        removedArray.push(arguments[i]);
+    };
+    
+    const filteredArray = array.filter(item => !removedArray.includes(item));
+    return filteredArray
 };
 
 // Do not edit below this line
