ea8e9c754950e6f655884893ce0d76940f8c43e6
alicecha <110170331+alicecha@users.noreply.github.com>
2023-01-17 18:08:47 +0200

    Write a function called `reverseString` that returns input, reversed!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    const stringLength = string.length;
+    let reversedString = "";
+
+    for(i=stringLength-1; i>=0; i--) {
+        reversedString += string.charAt(i);
+    };
+    console.log(reversedString);
+    return reversedString;
 
 };
 
