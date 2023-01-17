77c1407523463a6bbcdc2002b3e9aef0ad18759a
alicecha <110170331+alicecha@users.noreply.github.com>
2023-01-17 19:02:37 +0200

    Write 2 functions to convert Fahrenheit <=> Celsius temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  celsiustemp = (tempF-32)*(5/9);
+  //returns value rounded to 1 decimal point
+  return parseFloat(celsiustemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  fahrtemp = tempC*(9/5)+32;
+  //returns value rounded to 1 decimal point
+  return parseFloat(fahrtemp.toFixed(1));
 };
 
 // Do not edit below this line
