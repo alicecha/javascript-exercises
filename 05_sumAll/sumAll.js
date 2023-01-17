e566ffd62297bbdb11485be4ec6a1c95b979e029
alicecha <110170331+alicecha@users.noreply.github.com>
2023-01-17 18:44:07 +0200

    Create function that sums every number between a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    
+    //return error if negative numbers or if input isn't a number:
+    if(x < 0 || y < 0 || typeof x != 'number' || typeof y != 'number') {      
+        return "ERROR"
+    } else {
 
-};
+    //Assign smallest number to num1 and largest number to num2
+        const int1 = (x<=y) ? x : y;
+        const int2 = (int1 === x) ? y : x;
+
+        let intSum = 0;
+    
+    //Create loop to add all numbers within range:    
+        for (i=int1; i<=int2; i++) {
+            intSum += i;
+        };
+
+        return intSum;
+    };
+}
 
 // Do not edit below this line
 module.exports = sumAll;
